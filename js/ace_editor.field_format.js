6c322f5340dc9be16b41ed48c25ee88301bae60a
nylin <max@articstudios.se>
2012-01-24 13:18:13 +0100

    Issue #1414280 by nylin: Text format filter to output [editor][/editor] wrapped content into an Ace Editor

--- js/ace_editor.field_format.js
+++ js/ace_editor.field_format.js
@@ -3,10 +3,10 @@
   Drupal.behaviors.ace_editor_field_format = {
 	attach: function(context, settings) {
 		
-		var fieldSettings = Drupal.settings.ace_editor.field_format_settings;
+		var fieldFormatSettings = Drupal.settings.ace_editor.field_format_settings;
 		
-		var SyntaxMode = require("ace/mode/" + fieldSettings['syntax']).Mode;
-		var themePath = "ace/theme/" + fieldSettings['theme'];
+		var SyntaxMode = require("ace/mode/" + fieldFormatSettings['syntax']).Mode;
+		var themePath = "ace/theme/" + fieldFormatSettings['theme'];
 		$(Drupal.settings.ace_editor.field_format_instances).each(function(i) {
 			var $pre = $('#' + this['id']);
 			var html = this['content'];
@@ -14,14 +14,19 @@
 			var editor_instance = ace.edit(this['id']);
 			editor_instance.getSession().setMode(new SyntaxMode());
 			editor_instance.setTheme(themePath);
-			editor_instance.renderer.setShowGutter(fieldSettings['line_numbers']);
-			editor_instance.setShowPrintMargin(fieldSettings['print_margin']);
-			editor_instance.setShowInvisibles(fieldSettings['invisibles']);
+			editor_instance.renderer.setShowGutter(fieldFormatSettings['line_numbers']);
+			editor_instance.setShowPrintMargin(fieldFormatSettings['print_margin']);
+			editor_instance.setShowInvisibles(fieldFormatSettings['invisibles']);
 			editor_instance.setReadOnly(true);
-			$pre.css('font-size', fieldSettings['font_size']);
+			$pre.css({
+				'font-size': formatFilterSettings['font_size'],
+				'height': formatFilterSettings['field_height'],
+				'width': formatFilterSettings['field_width']
+			});
 			
 			editor_instance.getSession().setValue(html);
 		});
+		
 		
 	}
   };
