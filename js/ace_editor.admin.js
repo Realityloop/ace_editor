b1cd37b0a556b66ba0a75e50abee158171921251
interdruper <interdruper@2437374.no-reply.drupal.org>
2015-02-02 00:48:26 +0100

    Issue #1420290 by interdruper: Added autowrap and line highlighting options to admin panel.

--- js/ace_editor.admin.js
+++ js/ace_editor.admin.js
@@ -2,11 +2,11 @@
 
   Drupal.behaviors.ace_editor_admin = {
 	attach: function(context, settings) {
-		
+
 		// Contains the settings for the editors.
 		var editorSettings;
 		var formIdentifier;
-		
+
 		/**
 		* If the user selected a text format configured to be used with the editor,
 		* show it, else show the default textarea.
@@ -15,12 +15,12 @@
 			//console.log("Acify");
 			// The select list for chosing the text format that will be used.
 			var $filterSelector = $textFormatWrapper.find('select.filter-list');
-			
+
 			// Checks if the currently selected filter is one that uses the editor.
 			if ($.inArray($filterSelector.val(), Drupal.settings.ace_editor.admin.text_formats) != -1) {
-				
+
 				editorsJustAdded = false;
-				
+
 				/**
 				 * If the settings has not been set I will copy them into a new object.
 				 * if I don't the settings will be duplicated every time I add a textarea
@@ -32,54 +32,56 @@
 				}
 
 				// Check to see if the editor has been added yet.
-				if ($textFormatWrapper.find('div.ace-editor-container').length != 
+				if ($textFormatWrapper.find('div.ace-editor-container').length !=
 					$textFormatWrapper.find('div.form-item.form-type-textarea').length) {
-					
+
 					// Init the array that will hold all editor elements and instances inside
 					// of each text format wrapper.
 					var editors = new Array();
-					
+
 					// Iterate through all textarea containers that and attach the editor.
 					$('div.form-item.form-type-textarea:visible', $textFormatWrapper).each(function(i) {
 						var $form_item = $(this);
-						var editor_instance; 
-						
+						var editor_instance;
+
 						if (!$form_item.find('pre').length) {
 							// Create the editors container.
 							var $ace_editor_container = $('<div class="ace-editor-container"></div>');
 							// Put the different parts together.
 							var $pre = $('<pre id="' + $(this).find('textarea').attr('id') + '-aced"></pre>');
 							$ace_editor_container.append($pre);
-							
+
 							// Get the stored height for the fields if it exists.
 							var sizeId = formIdentifier + '_' + $form_item.find('textArea').attr('id').replace(new RegExp('-', 'g'), '_')
 							var storedHeight = localStorageGet('ace_editor_' + sizeId + '_height');
 							var height = storedHeight ? storedHeight : null;
-							
+
 							// Only append controlls to the main form item.
 							var $controls;
 							if ($(this).attr('class').indexOf("-value") != -1) {
-								
+
 								$pre.css({'height': height ? height : '500px'});
 								$controls = get_editor_controls();
 								$ace_editor_container.append($controls);
 								$(this).append($ace_editor_container);
 							} else { // It's a summary form item.
-								
+
 								$pre.css({'height': height ? height : '200px', 'border-bottom': '1px solid #CCC'});
 								$(this).find('div.form-textarea-wrapper').after($ace_editor_container);
 							}
-						
+
 							// Initialize the editor and set the correct options.
 							editor_instance = ace.edit($pre.attr('id'));
 							editor_instance.setTheme("ace/theme/" + editorSettings.theme);
 							var HTMLMode = ace.require("ace/mode/html").Mode;
 							editor_instance.getSession().setMode(new HTMLMode());
 							editor_instance.setShowPrintMargin(editorSettings['printmargin']);
+							editor_instance.getSession().setUseWrapMode(editorSettings['autowrap']);
+							editor_instance.setHighlightActiveLine(editorSettings['linehighlighting']);
 							editor_instance.renderer.setHScrollBarAlwaysVisible(false);
 							$pre.css('font-size', editorSettings['fontsize']);
 							$pre.data('editor_instance', editor_instance);
-							
+
 							// Adds handles to the editors for vertical resizing.
 							$pre.resizable({
 								handles: 's',
@@ -92,48 +94,48 @@
 									localStorageSet('ace_editor_' + sizeId + '_height', $pre.height());
 								}
 							});
-							
+
 							// Add event listeners.
 							editor_instance.getSession().on('change', function(editor) {
 								editorContentChange($form_item, editorObject);
 							});
-							
+
 							editorsJustAdded = true;
 						} else {
 							$pre = $form_item.find('pre');
 							editor_instance = $pre.data('editor_instance');
 						}
-						
+
 						var editorObject = {
 							editor: editor_instance,
 							element: $pre
 						};
 						editors.push(editorObject);
 					});
-					
+
 					// Store the newly created editor instances for later use
 					$textFormatWrapper.data('ace-editors', editors);
-					
+
 					// Set control states.
 					$textFormatWrapper.find('div.ace-editor-controls').each(function(i) {
 						var $controls = $(this);
-						
+
 						var $textArea = $textFormatWrapper.find('textarea');
 						var textAreaID = formIdentifier + '_' + $textArea.attr('id').replace(new RegExp('-', 'g'), '_');
-						
+
 						var showHidden = localStorageGet('ace_editor_' + textAreaID + '_show_hidden');
 						var lineNumbers = localStorageGet('ace_editor_' + textAreaID + '_show_line_numbers');
 						var mode = localStorageGet('ace_editor_' + textAreaID + '_mode');
-						
+
 						$controls.find('input.show_hidden').attr('checked', (showHidden == 1) ? true : false);
 						$controls.find('input.show_line_numbers').attr('checked', (lineNumbers == 1) ? true : false);
 						$controls.find('select.mode').val(mode ? mode : 'html');
-						
+
 						// Trigger controls.
 						$controls.find('div.control input, div.control select').trigger('change');
 					});
 				}
-				
+
 				// Get all of the editor objects for the text format wrapper.
 				var editorObjects;
 				if (editorsJustAdded) {
@@ -141,51 +143,51 @@
 				} else {
 					editorObjects = $textFormatWrapper.data('ace-editors');
 				}
-				
+
 				// Show the editor containers and hide the textarea containers.
 				$(editorObjects).each(function(i) {
 					var $formItem = $(this["element"]).parents('div.form-item:first');
 					var $textAreaWrapper = $formItem.find('div.form-textarea-wrapper');
-					
+
 					if ($textAreaWrapper.is(":visible")) {
 						var $editorContainer = $formItem.find('div.ace-editor-container');
-				
+
 						// Add the content of the field to the editors current session.
 						this["editor"].getSession().setValue($textAreaWrapper.find('textarea').val());
-				
+
 						$textAreaWrapper.hide();
 						$editorContainer.show();
 					}
 				});
-					
+
 			} else { // Show the textarea.
-				
+
 				var editorObjects = $textFormatWrapper.data('ace-editors');
 				if (editorObjects) {
-					
+
 					// Only switch of editors are shown.
 					var $firstElement = $(editorObjects[0]["element"]);
 					if ($firstElement.is(':visible') || !$firstElement.parent().parent().is(':visible')) {
 						$(editorObjects).each(function(i) {
 							var editorObj = editorObjects[i];
-						
+
 							var $formItem = $(editorObj["element"]).parents('div.form-item:first');
 							var $textAreaWrapper = $formItem.find('div.form-textarea-wrapper');
 							var $editorContainer = $formItem.find('div.ace-editor-container');
-						
+
 							// Transfer content from editor to textarea.
 							$textAreaWrapper.find('textarea').val(editorObj['editor'].getSession().getValue())
-						
+
 							// Hide the editor containers and show the textarea containers.
 							$textAreaWrapper.show();
 							$editorContainer.hide();
 						});
 					}
 				}
-				
+
 			}
 		}
-		
+
 		/**
 		* Bind the change event to all text format select lists.
 		*/
@@ -193,24 +195,24 @@
 			var $textFormatWrapper = $(this).parents('div.text-format-wrapper:first');
 			acifyWrapper($textFormatWrapper);
 		});
-			
+
 		/**
 		 * Update the editors to reflect the toggled option.
 		 */
 		$('div.text-format-wrapper div.control input, div.text-format-wrapper div.control select').live('change', function(e) {
-			
+
 			var $control = $(this);
 			var $textFormatWrapper = $(this).parents('div.text-format-wrapper:first');
 			var editorObjects = $textFormatWrapper.data('ace-editors');
 			var $textArea = $textFormatWrapper.find('textarea');
 			var textAreaID = formIdentifier + '_' + $textArea.attr('id').replace(new RegExp('-', 'g'), '_');
-					
+
 			var checked;
 			if ($control.attr('type') == 'checkbox') {
 				checked = $(this).is(':checked') ? 1 : 0;
 			}
-			
-			
+
+
 			if ($control.attr('name') == 'mode') {
 				if ($control.val() == 'html') {
 					// Apply settings to all editors.
@@ -239,7 +241,7 @@
 					}
 				});
 			}
-			
+
 			// Save settings 'till next time.
 			switch ($control.attr('name')) {
 				case "show_hidden":
@@ -253,7 +255,7 @@
 					break;
 			}
 		});
-		
+
 		/**
 		 * Add the editor to the summary field.
 		 * Seems that drupal blocks the use of click here, so we need to use
@@ -265,7 +267,7 @@
 				acifyWrapper($link.parents('div.text-format-wrapper:first'));
 			}, 10);
 		});
-		
+
 		/**
 		 * The content of the editor has changed, update the span showing line numbers.
 		 * Also transfer the content of the editors to their related textareas.
@@ -276,21 +278,21 @@
 		 */
 		function editorContentChange($form_item, editorObject) {
 			$(editorObject['element']).parents('div.ace-editor-container:first').find('div.ace-editor-controls span.num-lines').text(editorObject['editor'].getSession().getValue().split("\n").length + " lines");
-			
+
 			var $textarea = $form_item.find('textarea');
 			var val = editorObject['editor'].getSession().getValue();
 			$textarea.text(val);
 		}
-	
-		
+
+
 		/**
 		*	Returns the controls for an editor.
 		*/
 		function get_editor_controls() {
 			$controls = $('<div class="ace-editor-controls"></div>');
-			$controls.append('<div class="control"><input type="checkbox" name="show_hidden" class="show_hidden" checked>' + 
+			$controls.append('<div class="control"><input type="checkbox" name="show_hidden" class="show_hidden" checked>' +
 							  '<label>Invisibles</label></div>');
-			$controls.append('<div class="control"><input type="checkbox" name="show_line_numbers" class="show_line_numbers" checked>' + 
+			$controls.append('<div class="control"><input type="checkbox" name="show_line_numbers" class="show_line_numbers" checked>' +
 							  '<label>Line numbers</label></div>');
 			$modes_select = $('<div class="control"><select name="mode" class="mode"></select></div>');
 			$.each(editorSettings['available_modes'], function(key, value) {
@@ -298,12 +300,12 @@
 				$('select', $modes_select).append('<option value="' + key + '"' + selected + '>' + value + '</option>');
 			});
 			$controls.append($modes_select);
-							
+
 			$controls.append('<div class="info"><span class="num-lines">1 lines</span><a class="key-bindings" target="_blank" href="https://github.com/ajaxorg/ace/wiki/Default-Keyboard-Shortcuts">Show key bindings</a></div>');
-			
+
 			return $controls;
 		}
-		
+
 		/**
 		 * Saves data to localStorage if available.
 		 */
@@ -312,7 +314,7 @@
 				localStorage[$key] = $value;
 			}
 		}
-		
+
 		/**
 		 * Gets data from localStorage if available.
 		 */
@@ -320,10 +322,10 @@
 			if (localStorageAvailable()) {
 				return localStorage[$key];
 			}
-			
+
 			return null
 		}
-		
+
 		/**
 		 *
 		 */
--- js/ace_editor.js
+++ js/ace_editor.js
@@ -6,15 +6,17 @@
                               var $pre = $('#' + this['id']);
                               var html = this['content'];
                               var settings = this['settings'];
-                              
+
                               var SyntaxMode = ace.require("ace/mode/" + settings['syntax']).Mode;
                               var themePath = "ace/theme/" + settings['theme'];
-                              
+
                               var editor_instance = ace.edit(this['id']);
                               editor_instance.getSession().setMode(new SyntaxMode());
                               editor_instance.setTheme(themePath);
                               editor_instance.renderer.setShowGutter(settings['line_numbers']);
                               editor_instance.setShowPrintMargin(settings['print_margin']);
+                              editor_instance.getSession().setUseWrapMode(settings['autowrap']);
+                              editor_instance.setHighlightActiveLine(settings['linehighlighting']);
                               editor_instance.setShowInvisibles(settings['invisibles']);
                               editor_instance.renderer.setHScrollBarAlwaysVisible(false);
                               editor_instance.setReadOnly(true);
@@ -22,22 +24,22 @@
                                     'font-size': settings['font_size'],
                                     'width': settings['width']
                               });
-                              
-                              //We need to put the content to calcuate the height
+
+                              //We need to put the content to calculate the height
                               editor_instance.getSession().setValue(html);
-                              
+
                               //Deal with the height.
                               //If auto.
                               if (settings['height'] == 'auto') {
                                     //The height is number of lines * line height + height on the scrollbar (if needed) + 5px (to keep everything less squished).
                                     //As far as I know, there is no way of knowing if the scrollbar is present or not because Ace hasn't rendered the editor yet.
                                     //So, we're going to create our own <span> with the content in it, hide it, and check if it fits in the PRE.
-                                    
-                                    //Create the hidden span, append it to the PRE and get its width with nowrap.    
+
+                                    //Create the hidden span, append it to the PRE and get its width with nowrap.
                                     var content_width = $('<span></span>').attr('id', 'ace_editor_to_remove').css({display:'none',whiteSpace:'nowrap'}).appendTo($('#'+editor_instance.container.id)).text(editor_instance.getSession().getValue()).width();
                                     $('#ace_editor_to_remove').remove(); //It's now useless, remove it.
                                     var pre_width = $('#'+editor_instance.container.id).width(); //We HAVE to re-take the width because it can be set in % in the config.
-                                    
+
                                     if (content_width > pre_width) {
                                           $pre.css('height', ((editor_instance.renderer.lineHeight) * (editor_instance.getSession().getValue().split('\n').length) + editor_instance.renderer.scrollBarH.height) + 5);
                                     }else{
